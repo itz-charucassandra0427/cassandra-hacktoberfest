6700bf0cda1501cd7b32ba3b895c285216ee9a39
eggsarenice <69945192+eggsarenice@users.noreply.github.com>
2020-10-20 14:54:38 +0800

    Updated

--- cassandra.py
+++ cassandra.py
@@ -1,4 +1,4 @@
-print("Title of program: CCA Matching Personality test")
+print("Title of program: CCA Matching Personality tests")
 print()
 print("Welcome to DHS! Please answer the following questions truthfully and we'll suggest a CCA for you!")
 print("Please respond with a number 1 - 5, where 1 is strongly disagree and 5 is strongly agree.")
